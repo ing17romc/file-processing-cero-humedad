4bbe2572573f338b70a8328084e7bd9f54c6c0c7
Rafael Márquez <ing17romc@gmail.com>
2023-03-19 12:48:18 -0600

    updated const

--- constants.js
+++ constants.js
@@ -3,7 +3,7 @@ module.exports = {
     NAME: 'initial_load/default.xlsx',
     SHEET: 2
   },
-  PATH: '../../Dropbox/autoservicios/cero-humedad/walmart/2022',
+  PATH: '../../Dropbox/autoservicios/cero-humedad/walmart/2023',
   START_ROW: 21,
   ITEM_DEFAULT: { ID: 1, NOMBRE: 'SIN ASIGNAR', ESTADO: 0 },
   TYPE_LOG: {
